438929b94dc16c1ea01c726c639aea4f514a8966
Priyankakrishna3 <priyankakrishnaw@gmail.com>
2020-05-01 16:37:09 +0530

    created new project

---
+++ src/test/java/com/selenium/testscripts/TLTestcase.java
@@ -0,0 +1,12 @@
+package com.selenium.testscripts;
+
+import org.testng.annotations.Test;
+
+public class TLTestcase {
+	@Test
+	public void welcome()
+	{
+		System.out.println("welcome to new project");
+	}
+
+}
