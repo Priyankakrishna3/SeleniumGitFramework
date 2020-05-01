cd7f5a832c73ea6b0772923e2924a937ebfe7859
Priyankakrishna3 <priyankakrishnaw@gmail.com>
2020-05-01 17:20:58 +0530

    "adding first testcase"

--- src/test/java/com/selenium/testscripts/ATE1Testcase.java
+++ src/test/java/com/selenium/testscripts/ATE1Testcase.java
@@ -1,5 +1,12 @@
 package com.selenium.testscripts;
 
+import org.testng.annotations.Test;
+
 public class ATE1Testcase {
+	@Test
+	public void testCase1()
+	{
+		System.out.println("Test case 1 ATE1");
+	}
 
 }
